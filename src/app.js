0a42388125ca6580dd43da24a27ed0532f082c88
JetsadaWijit <jetsadawijit@outlook.com>
2025-03-02 11:55:22 +0000

    Set simple code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Function
+const { ensureFileExists } = require('./utils/fileHandler')
+
+// Define the file path (data/users.sqlite)
+const filePath = path.join(__dirname, 'data', 'users.sqlite')
+
+// Ensure the file exists
+ensureFileExists(filePath);
+
+// App resources
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('learning', require('./routes/learning'));
+app.use('/login', require('./routes/login'));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
