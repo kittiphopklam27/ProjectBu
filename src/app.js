c183295f988f9c2339dbf319b44d0feee909e163
JetsadaWijit <jetsadawijit@outlook.com>
2025-03-02 11:59:43 +0000

    Move view engine line

--- src/app.js
+++ src/app.js
@@ -16,9 +16,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/home'));
